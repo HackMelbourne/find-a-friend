5b93d3563bfd7b17412de3c3ccad4555c34865c0
Matthew Kwon <hackmelb.team@gmail.com>
2021-03-10 21:37:48 +1100

    lesson-0

---
+++ src/pages/SignUp.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class SignUp extends React.Component {
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default SignUp;
